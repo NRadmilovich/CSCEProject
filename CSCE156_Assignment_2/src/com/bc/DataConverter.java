d7fbeb5ab780b4e3743d36001f451bbff96f8688
ckirb21 <59975290+ckirb21@users.noreply.github.com>
2020-09-16 17:36:59 -0500

    Update DataConverter.java

--- CSCE156_Assignment_2/src/com/bc/DataConverter.java
+++ CSCE156_Assignment_2/src/com/bc/DataConverter.java
@@ -3,18 +3,16 @@ package com.bc;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class DataConverter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		// Data Conversion for Products
-		ArrayList<Rental> rentals = null;
-		ArrayList<Repair> repairs = null;
-		ArrayList<Concession> concessions = null;
-		ArrayList<Towing> tows = null;
+		List<Products> products  = new ArrayList<Products>();
 		
 		Scanner s;
 		
@@ -29,22 +27,22 @@ public class DataConverter {
 				if (tokens[1].compareTo("R") == 0) {
 					Rental r = null;
 					r = new Rental(tokens[0], tokens[1], tokens[2], Double.parseDouble(tokens[3]), Double.parseDouble(tokens[4]), Double.parseDouble(tokens[5]));		
-					rentals.add(r);
+					products.add(r);
 					
 				} else if (tokens[1].compareTo("F") == 0) {
 					Repair f = null;
 					f = new Repair(tokens[0], tokens[1], tokens[2], Double.parseDouble(tokens[3]), Double.parseDouble(tokens[4]));		
-					repairs.add(f);
+					products.add(f);
 					
 				} else if (tokens[1].compareTo("C") == 0) {
 					Concession c = null;
 					c = new Concession(tokens[0], tokens[1], tokens[2], Double.parseDouble(tokens[3]));		
-					concessions.add(c);
+					products.add(c);
 					
 				} else if (tokens[1].compareTo("T") == 0) {
 					Towing t = null;
 					t = new Towing(tokens[0], tokens[1], tokens[2], Double.parseDouble(tokens[3]));		
-					tows.add(t);
+					products.add(t);
 					
 				}
 				
@@ -57,10 +55,7 @@ public class DataConverter {
 		}
 		
 		// Print json to file
-		JsonWrite.printJSON("data/Products.json", rentals);
-		JsonWrite.printJSON("data/Products.json", repairs);
-		JsonWrite.printJSON("data/Products.json", concessions);
-		JsonWrite.printJSON("data/Products.json", tows);
+		JsonWrite.printJSON("data/Products.json", products);
 	}
 
 }
