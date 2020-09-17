8b9dde05407fdffcb8e22cdf26ea1d30668ef291
Nradmilovich4 <71038540+Nradmilovich4@users.noreply.github.com>
2020-09-16 19:34:30 -0500

    Updating DataConverter

--- CSCE156_Assignment_2/src/com/bc/DataConverter.java
+++ CSCE156_Assignment_2/src/com/bc/DataConverter.java
@@ -3,6 +3,7 @@ package com.bc;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import com.google.gson.*;
 import java.util.HashMap;
@@ -15,13 +16,10 @@ public class DataConverter {
 		ArrayList<Person> people = Person.importPerson("Persons.dat");
 		HashMap<String,Person> personMap = Person.personMap(people);
 		ArrayList<Customer> customers = Customer.importCustomer("Customers.dat", personMap);
-		
+
 		
 		// Data Conversion for Products
-		ArrayList<Rental> rentals = null;
-		ArrayList<Repair> repairs = null;
-		ArrayList<Concession> concessions = null;
-		ArrayList<Towing> tows = null;
+		List<Products> products  = new ArrayList<Products>();
 		
 		Scanner s;
 		
@@ -36,22 +34,22 @@ public class DataConverter {
 				if (tokens[1].compareTo("R") == 0) {
 					Rental r = null;
 					r = new Rental(tokens[0], tokens[1], tokens[2], Double.parseDouble(tokens[3]), Double.parseDouble(tokens[4]), Double.parseDouble(tokens[5]));		
-					rentals.add(r);
+					products.add(r);
 					
 				} else if (tokens[1].compareTo("F") == 0) {
 					Repair f = null;
 					f = new Repair(tokens[0], tokens[1], tokens[2], Double.parseDouble(tokens[3]), Double.parseDouble(tokens[4]));		
-					repairs.add(f);
+					products.add(f);
 					
 				} else if (tokens[1].compareTo("C") == 0) {
 					Concession c = null;
 					c = new Concession(tokens[0], tokens[1], tokens[2], Double.parseDouble(tokens[3]));		
-					concessions.add(c);
+					products.add(c);
 					
 				} else if (tokens[1].compareTo("T") == 0) {
 					Towing t = null;
 					t = new Towing(tokens[0], tokens[1], tokens[2], Double.parseDouble(tokens[3]));		
-					tows.add(t);
+					products.add(t);
 					
 				}
 				
@@ -66,10 +64,6 @@ public class DataConverter {
 		XMLPrint.printXML("data/Persons.xml", people, "Person");
 		XMLPrint.printXML("data/Customers.xml", customers, "Customer");
 		// Print json to file
-		JsonWrite.printJSON("data/Products.json", rentals);
-		JsonWrite.printJSON("data/Products.json", repairs);
-		JsonWrite.printJSON("data/Products.json", concessions);
-		JsonWrite.printJSON("data/Products.json", tows);
 		JsonWrite.printJSON("data/Products.json", products);
 	}
 
