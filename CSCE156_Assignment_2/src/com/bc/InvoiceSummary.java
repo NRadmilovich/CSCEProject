cf48162a14c041c65631a1363591543554814654
ckirb21 <59975290+ckirb21@users.noreply.github.com>
2020-09-29 11:49:15 -0500

    added InvoiceSummary class

---
+++ CSCE156_Assignment_2/src/com/bc/InvoiceSummary.java
@@ -0,0 +1,23 @@
+package com.bc;
+
+import java.util.Collection;
+
+public class InvoiceSummary {
+	
+	public static void printInvoice() {
+
+		
+		
+		System.out.println("Executive Summary Report:");
+		System.out.printf("%n%-10s %-25s %-30s %-10s %-10s  %-10s  %-10s  %-10s %n", "Code", "Owner", "Customer Account", "Subtotal", "Discounts", "Fees", "Taxes", "Total");
+		System.out.println("----------------------------------------------------------------------------------------------------------------------------");
+		
+		// TODO Loop through collection of invoices
+		// Call computation interface for each invoice
+		
+		
+		System.out.println("============================================================================================================================");
+
+	}
+
+}
