8a15509e8ea6680ac5fc05307827b4d0f671e50a
ckirb21 <59975290+ckirb21@users.noreply.github.com>
2020-10-15 14:56:49 -0500

    added Product and InvoiceProduct tables

--- CSCE156_Assignment_2/SQL Code/Assignment4Script.sql
+++ CSCE156_Assignment_2/SQL Code/Assignment4Script.sql
@@ -1,13 +1,14 @@
 use nradmilo;
 
+drop table if exists InvoiceProduct;
 drop table if exists Invoice;
+drop table if exists Product;
 drop table if exists Customer;
 drop table if exists PersonAddress;
 drop table if exists PersonEmail;
 drop table if exists Person;
 drop table if exists Email;
 drop table if exists Address;
-
 
 Create table Address(
 addressId int not null primary key auto_increment,
@@ -55,6 +56,25 @@ foreign key (primaryContact) references Person(personId),
 foreign key (address) references Address(addressId),
 unique (customerId));
 
+create table Product (
+-- All Products
+productId int not null primary key auto_increment,
+productCode varchar(255) not null,
+productType varchar(1) not null,
+productLabel varchar(255) not null,
+-- Rental
+dailyCost double default null,
+deposit double default null,
+cleaningFee double default null,
+-- Repair
+partsCost double default null,
+hourlyLaborCost double default null,
+-- Towing
+costPerMile double default null,
+-- Concession
+unitCost double default null,
+associatedRepair boolean default false);
+
 create table Invoice(
 invoiceId int not null primary key auto_increment,
 invoiceCode varchar(30) not null,
@@ -63,3 +83,13 @@ customer int not null,
 foreign key (owner) references Person(personId),
 foreign key (customer) references Customer(customerId),
 unique (invoiceCode));
+
+create table InvoiceProduct (
+invoiceProductId int primary key not null auto_increment,
+workValue double not null,
+
+invoiceId int not null,
+foreign key (invoiceId) references Invoice(invoiceId),
+    
+productId int not null,
+foreign key (productId) references Product(productId));
