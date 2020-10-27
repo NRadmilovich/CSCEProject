bc1c23d0f82d7ae38464c0a39a2b9e18f523287f
ckirb21 <59975290+ckirb21@users.noreply.github.com>
2020-10-26 20:39:21 -0500

    implemented helper methods

--- CSCE156_Assignment_2/src/com/bc/ext/InvoiceData.java
+++ CSCE156_Assignment_2/src/com/bc/ext/InvoiceData.java
@@ -23,26 +23,26 @@ import com.bc.model.Repair;
 
 /**
  * This is a collection of utility methods that define a general API for
- * interacting with the database supporting this application.
- * 16 methods in total, add more if required.
- * Do not change any method signatures or the package name.
+ * interacting with the database supporting this application. 16 methods in
+ * total, add more if required. Do not change any method signatures or the
+ * package name.
  * 
  * Nick - 1,2,3,4,5,11,12,15
  * 
  * Adapted from Dr. Hasan's original version of this file
+ * 
  * @author Chloe
  *
  */
 public class InvoiceData {
 
 	/**
-	 * 1. Method that removes every person record from the database
-	 *  Delete person and address
-	 *  Do they want the customer contact null, or
-	 *  to keep the person code on file?
+	 * 1. Method that removes every person record from the database Delete person
+	 * and address Do they want the customer contact null, or to keep the person
+	 * code on file?
 	 */
 	public static void removeAllPersons() {
-		/* TODO*/
+		/* TODO */
 	}
 
 	/**
@@ -57,8 +57,9 @@ public class InvoiceData {
 	 * @param zip
 	 * @param country
 	 */
-	public static void addPerson(String personCode, String firstName, String lastName, String street, String city, String state, String zip, String country) {
-		/* TODO*/
+	public static void addPerson(String personCode, String firstName, String lastName, String street, String city,
+			String state, String zip, String country) {
+		/* TODO */
 	}
 
 	/**
@@ -69,17 +70,17 @@ public class InvoiceData {
 	 * @param email
 	 */
 	public static void addEmail(String personCode, String email) {
-		/* TODO*/
+		/* TODO */
 	}
 
 	/**
-	 * 4. Method that removes every customer record from the database
-	 * Do they want customer Id saved in invoices, or null?
+	 * 4. Method that removes every customer record from the database Do they want
+	 * customer Id saved in invoices, or null?
 	 */
 	public static void removeAllCusomters() {
-		/* TODO*/
+		/* TODO */
 	}
-	
+
 	/**
 	 * 5. Method to add a customer record to the database with the provided data
 	 * 
@@ -95,15 +96,17 @@ public class InvoiceData {
 	 * @param zip
 	 * @param country
 	 */
-	public static void addCustomer(String customerCode, String customerType, String primaryContactPersonCode, String name, String street, String city, String state, String zip, String country) {
-		/* TODO*/
+	public static void addCustomer(String customerCode, String customerType, String primaryContactPersonCode,
+			String name, String street, String city, String state, String zip, String country) {
+		/* TODO */
 	}
-	
+
 	/**
 	 * 6. Removes all product records from the database
 	 */
 	public static void removeAllProducts() {
-		/* TODO*/
+		/* TODO */
+
 	}
 
 	/**
@@ -114,7 +117,12 @@ public class InvoiceData {
 	 * @param unitCost
 	 */
 	public static void addConcession(String productCode, String productLabel, double unitCost) {
-	
+
+		// Checks for duplicate
+		int productId = InvoiceData.getProductId(productCode);
+		if (productId != 0) {
+			return;
+		}
 		String query = "insert into Product (productCode, productType, productLabel, unitCost) values (?,'C',?,?);";
 		PreparedStatement pre = null;
 		Connection conn = DatabaseConnection.connectionBuilder();
@@ -124,7 +132,7 @@ public class InvoiceData {
 			pre.setString(2, productLabel);
 			pre.setDouble(3, unitCost);
 			pre.executeUpdate();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -141,7 +149,12 @@ public class InvoiceData {
 	 * @param laborRate
 	 */
 	public static void addRepair(String productCode, String productLabel, double partsCost, double laborRate) {
-		
+
+		// Checks for duplicate
+		int productId = InvoiceData.getProductId(productCode);
+		if (productId != 0) {
+			return;
+		}
 		String query = "insert into Product (productCode, productType, productLabel, partsCost, hourlyLaborCost) values (?,'F',?,?,?);";
 		PreparedStatement pre = null;
 		Connection conn = DatabaseConnection.connectionBuilder();
@@ -152,7 +165,7 @@ public class InvoiceData {
 			pre.setDouble(3, partsCost);
 			pre.setDouble(4, laborRate);
 			pre.executeUpdate();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -168,7 +181,12 @@ public class InvoiceData {
 	 * @param costPerMile
 	 */
 	public static void addTowing(String productCode, String productLabel, double costPerMile) {
-		
+
+		// Checks for duplicate
+		int productId = InvoiceData.getProductId(productCode);
+		if (productId != 0) {
+			return;
+		}
 		String query = "insert into Product (productCode, productType, productLabel, costPerMile) values (?,'T',?,?);";
 		PreparedStatement pre = null;
 		Connection conn = DatabaseConnection.connectionBuilder();
@@ -178,7 +196,7 @@ public class InvoiceData {
 			pre.setString(2, productLabel);
 			pre.setDouble(3, costPerMile);
 			pre.executeUpdate();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -195,8 +213,14 @@ public class InvoiceData {
 	 * @param deposit
 	 * @param cleaningFee
 	 */
-	public static void addRental(String productCode, String productLabel, double dailyCost, double deposit, double cleaningFee) {
-		
+	public static void addRental(String productCode, String productLabel, double dailyCost, double deposit,
+			double cleaningFee) {
+
+		// Checks for duplicate
+		int productId = InvoiceData.getProductId(productCode);
+		if (productId != 0) {
+			return;
+		}
 		String query = "insert into Product (productCode, productType, productLabel, dailyCost, deposit, cleaningFee) values (?,'R',?,?,?,?);";
 		PreparedStatement pre = null;
 		Connection conn = DatabaseConnection.connectionBuilder();
@@ -208,7 +232,7 @@ public class InvoiceData {
 			pre.setDouble(4, deposit);
 			pre.setDouble(5, cleaningFee);
 			pre.executeUpdate();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -222,7 +246,7 @@ public class InvoiceData {
 	 * 11. Removes all invoice records from the database
 	 */
 	public static void removeAllInvoices() {
-        /* TODO*/
+		/* TODO */
 	}
 
 	/**
@@ -233,27 +257,32 @@ public class InvoiceData {
 	 * @param customertCode
 	 */
 	public static void addInvoice(String invoiceCode, String ownerCode, String customerCode) {
-		/* TODO*/
+		/* TODO */
+		// Checks for duplicate
+		int invoiceId = InvoiceData.getInvoiceId(invoiceCode);
+		if (invoiceId != 0) {
+			return;
+		}
 	}
 
 	/**
-	 * 13. Adds a particular Towing (corresponding to <code>productCode</code>
-	 * to an invoice corresponding to the provided <code>invoiceCode</code> with
-	 * the given number of miles towed
+	 * 13. Adds a particular Towing (corresponding to <code>productCode</code> to an
+	 * invoice corresponding to the provided <code>invoiceCode</code> with the given
+	 * number of miles towed
 	 * 
 	 * @param invoiceCode
 	 * @param productCode
 	 * @param milesTowed
 	 */
-	public static void addTowingToInvoice(String invoiceCode, String productCode, double milesTowed) {	
+	public static void addTowingToInvoice(String invoiceCode, String productCode, double milesTowed) {
 		String repairCode = null;
 		InvoiceData.addProductToInvoice(invoiceCode, productCode, milesTowed, repairCode);
 	}
 
 	/**
-	 * 14. Adds a particular Repair (corresponding to <code>productCode</code>
-	 * to an invoice corresponding to the provided <code>invoiceCode</code> with
-	 * the given number of hours worked
+	 * 14. Adds a particular Repair (corresponding to <code>productCode</code> to an
+	 * invoice corresponding to the provided <code>invoiceCode</code> with the given
+	 * number of hours worked
 	 * 
 	 * @param invoiceCode
 	 * @param productCode
@@ -264,51 +293,83 @@ public class InvoiceData {
 		InvoiceData.addProductToInvoice(invoiceCode, productCode, hoursWorked, repairCode);
 	}
 
-     /**
-      * 15. Adds a particular Concession (corresponding to <code>productCode</code> to an 
-      * invoice corresponding to the provided <code>invoiceCode</code> with the given
-      * number of quantity.
-      * NOTE: repairCode may be null
-      * 
-      * 
-      * Change associated repair from boolean to string
-      * 
-      * @param invoiceCode
-      * @param productCode
-      * @param quantity
-      * @param repairCode
-      */
-    public static void addConcessionToInvoice(String invoiceCode, String productCode, int quantity, String repairCode) {
+	/**
+	 * 15. Adds a particular Concession (corresponding to <code>productCode</code>
+	 * to an invoice corresponding to the provided <code>invoiceCode</code> with the
+	 * given number of quantity. NOTE: repairCode may be null
+	 * 
+	 * 
+	 * Change associated repair from boolean to string
+	 * 
+	 * @param invoiceCode
+	 * @param productCode
+	 * @param quantity
+	 * @param repairCode
+	 */
+	public static void addConcessionToInvoice(String invoiceCode, String productCode, int quantity, String repairCode) {
 		InvoiceData.addProductToInvoice(invoiceCode, productCode, quantity, repairCode);
+	}
 
-    }
-	
-    /**
-     * 16. Adds a particular Rental (corresponding to <code>productCode</code> to an 
-     * invoice corresponding to the provided <code>invoiceCode</code> with the given
-     * number of days rented. 
-     * 
-     * @param invoiceCode
-     * @param productCode
-     * @param daysRented
-     */
-    public static void addRentalToInvoice(String invoiceCode, String productCode, double daysRented) {
-    	String repairCode = null;
+	/**
+	 * 16. Adds a particular Rental (corresponding to <code>productCode</code> to an
+	 * invoice corresponding to the provided <code>invoiceCode</code> with the given
+	 * number of days rented.
+	 * 
+	 * @param invoiceCode
+	 * @param productCode
+	 * @param daysRented
+	 */
+	public static void addRentalToInvoice(String invoiceCode, String productCode, double daysRented) {
+		String repairCode = null;
 		InvoiceData.addProductToInvoice(invoiceCode, productCode, daysRented, repairCode);
-    }
-    
-    /**
-     * Extra: generalizes adding a product and
-     * is called by each addToInvoice method for products
-     * 
-     * @param invoiceCode
-     * @param productCode
-     * @param workValue
-     */
-    public static void addProductToInvoice(String invoiceCode, String productCode, double workValue, String repairCode) {
-    	String query = "select productId from Product where productCode = ?;";
-		String query2 = "select invoiceId from Invoice where invoiceCode = ?;";
-		int productId = 0, invoiceId = 0;
+	}
+
+	/**
+	 * Helper: generalizes adding a product and is called by each addToInvoice
+	 * method for products
+	 * 
+	 * @param invoiceCode
+	 * @param productCode
+	 * @param workValue
+	 */
+	private static void addProductToInvoice(String invoiceCode, String productCode, double workValue,
+			String repairCode) {
+
+		String query = "insert into InvoiceProduct(invoiceId, productId, workValue, associatedRepair) values (?,?,?,?);";
+		int productId = InvoiceData.getProductId(productCode);
+		int invoiceId = InvoiceData.getInvoiceId(invoiceCode);
+		PreparedStatement pre = null;
+		Connection conn = DatabaseConnection.connectionBuilder();
+		try {
+			pre = conn.prepareStatement(query);
+			pre.setInt(1, invoiceId);
+			pre.setInt(2, productId);
+			pre.setDouble(3, workValue);
+			// Checks for the existence of an associatedRepair
+			if (repairCode != null) {
+				pre.setBoolean(4, true);
+			} else {
+				pre.setBoolean(4, false);
+			}
+			pre.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		DatabaseConnection.close(conn);
+		DatabaseConnection.close(pre);
+	}
+
+	/**
+	 * Helper: gets productId for use in checking for duplication and adding
+	 * Products to an Invoice
+	 * 
+	 * @param productCode
+	 * @return
+	 */
+	private static int getProductId(String productCode) {
+		String query = "select productId from Product where productCode = ?;";
+		int productId = 0;
 		PreparedStatement pre = null;
 		ResultSet rs = null;
 		Connection conn = DatabaseConnection.connectionBuilder();
@@ -318,34 +379,45 @@ public class InvoiceData {
 			rs = pre.executeQuery();
 			if (rs.next()) {
 				productId = rs.getInt("productId");
-			} 	
-			
-			pre = conn.prepareStatement(query2);
-			pre.setString(1, invoiceCode);
-			rs = pre.executeQuery();
-			if (rs.next()) {
-				invoiceId = rs.getInt("invoiceId");
-			} 	
-			
-			query = "insert into InvoiceProduct(invoiceId, productId, workValue, associatedRepair) values (?,?,?,?);";
-			pre = conn.prepareStatement(query);
-			pre.setInt(1, invoiceId);
-			pre.setInt(2, productId);
-			pre.setDouble(3, workValue);
-			if (repairCode != null) {
-				pre.setBoolean(4, true);
-			} else {
-				pre.setBoolean(4, false);
 			}
-			pre.executeUpdate();
-			
-			
-			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		DatabaseConnection.close(conn);
 		DatabaseConnection.close(pre);
 		DatabaseConnection.close(rs);
-    }
+
+		return productId;
+	}
+
+	/**
+	 * Helper: gets invoiceId for use in checking for duplication and adding things
+	 * to an Invoice
+	 * 
+	 * @param productCode
+	 * @return
+	 */
+	private static int getInvoiceId(String invoiceCode) {
+		String query = "select invoiceId from Invoice where invoiceCode = ?;";
+		int invoiceId = 0;
+		PreparedStatement pre = null;
+		ResultSet rs = null;
+		Connection conn = DatabaseConnection.connectionBuilder();
+		try {
+			pre = conn.prepareStatement(query);
+			pre.setString(1, invoiceCode);
+			rs = pre.executeQuery();
+			if (rs.next()) {
+				invoiceId = rs.getInt("invoiceId");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		DatabaseConnection.close(conn);
+		DatabaseConnection.close(pre);
+		DatabaseConnection.close(rs);
+
+		return invoiceId;
+	}
+
 }
